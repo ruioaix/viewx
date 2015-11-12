2358058e00affb0e88ce7e1aa401beb1b6bec792
RuiX <xrfind@gmail.com>
2015-11-12 11:37:08 +0100

    proxy error

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -18,7 +18,7 @@ class ProxyController extends Controller
         $now = time();
         $ago = $now - $step * $stepNum;
         $proxies = Proxy::time($ago);
-        $data = array(
+        $pm = array(
             'labels' => array(),
             'datasets' => array(
                 array(
@@ -57,56 +57,60 @@ class ProxyController extends Controller
             $ts = $ago + $step * ($i + 1);
             $dt = new \DateTime("@$ts");
             $dt->setTimeZone(new \DateTimeZone('Europe/Zurich'));
-            $data['labels'][$i] = $dt->format("H:i");
-            $data['datasets'][0]['data'][$i] = 0;
-            $data['datasets'][1]['data'][$i] = 0;
-            $data['datasets'][2]['data'][$i] = 0;
+            $pm['labels'][$i] = $dt->format("H:i");
+            $pm['datasets'][0]['data'][$i] = 0;
+            $pm['datasets'][1]['data'][$i] = 0;
+            $pm['datasets'][2]['data'][$i] = 0;
         }
+        $msg = array();
+        $msgcount = 1;
         foreach ($proxies as $proxy) {
             $start = $proxy['start_time'];
             $end = $proxy['end_time'];
+            $message = $proxy['message'];
+            if (isset($msg[$message])) {
+                $msg[$message] += 1;
+                $msgcount += 1;
+            }
+            else {
+                $msg[$message] = 1;
+                $msgcount += 1;
+            }
             if ($start == -1) {
                 $i = (int) (($end - $ago) / $step);
-                $data['datasets'][2]['data'][$i] += 1;
+                $pm['datasets'][2]['data'][$i] += 1;
             }
-            elseif ($proxy['message'] == 'success') {
+            elseif ($message == 'success') {
                 $i = (int) (($start - $ago) / $step);
-                $data['datasets'][1]['data'][$i] += 1;
+                $pm['datasets'][1]['data'][$i] += 1;
             }
             else {
                 $i = (int) (($start - $ago) / $step);
-                $data['datasets'][0]['data'][$i] += 1;
+                $pm['datasets'][0]['data'][$i] += 1;
             }
         }
-        $proxies = "hello world";
+        $pm = json_encode($pm);
 
-        #$data = array(
-        #    'labels' => array("January", "February", "March", "April", "May", "June", "July"),
-        #    'datasets' => array(
-        #        array( 
-        #            'label' => "My First dataset",
-        #            'fillColor' => "rgba(220,220,220,0.2)",
-        #            'strokeColor' => "rgba(220,220,220,1)",
-        #            'pointColor' => "rgba(220,220,220,1)",
-        #            'pointStrokeColor' => "#fff",
-        #            'pointHighlightFill' => "#fff",
-        #            'pointHighlightStroke' => "rgba(220,220,220,1)",
-        #            'data' => array(65, 59, 80, 81, 56, 55, 40)
-        #        ),
-        #        array(
-        #            'label'=> "My Second dataset",
-        #            'fillColor'=> "rgba(151,187,205,0.2)",
-        #            'strokeColor'=> "rgba(151,187,205,1)",
-        #            'pointColor'=> "rgba(151,187,205,1)",
-        #            'pointStrokeColor'=> "#fff",
-        #            'pointHighlightFill'=> "#fff",
-        #            'pointHighlightStroke'=> "rgba(151,187,205,1)",
-        #            'data'=> array(28, 48, 40, 19, 86, 27, 90)
-        #        )
-        #    )
-        #);
-        $data = json_encode($data);
-        return view('proxy.main', compact('proxies', 'data'));
+        $pe = array(
+            'labels' => array(),
+            'datasets' => array(
+                array(
+                    'label'=> "error",
+                    'fillColor' => "rgba(151,187,205,0.5)",
+                    'strokeColor' => "rgba(151,187,205,0.8)",
+                    'highlightFill' => "rgba(151,187,205,0.75)",
+                    'highlightStroke' => "rgba(151,187,205,1)",
+                    'data'=> array()
+                ),
+            ),
+        );
+        unset($msg['begin']);
+        arsort($msg);
+        foreach ($msg as $kind => $count) {
+            $pe['labels'][] = $kind . '(' . number_format($count/$msgcount * 100, 1) . '%)';
+            $pe['datasets'][0]['data'][] = $count;
+        }
+        $pe = json_encode($pe);
+        return view('proxy.main', compact('pm', 'pe'));
     }
-
 }
--- resources/views/proxy/main.blade.php
+++ resources/views/proxy/main.blade.php
@@ -4,14 +4,23 @@
 @endsection
 
 @section('content')
-<h2>Proxy Monitor: </h2>
-<canvas id="myChart" width="1140" height="400"></canvas>
+
+<h2>Proxy Monitor: (50 hours)</h2>
+<canvas id="proxy_monitor" width="1140" height="400"></canvas>
 <script type="text/javascript">
 $(document).ready(function() {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    new Chart(ctx).Line({!! $data !!}, {scaleLabel: function(object){return " " + object.value; }, pointHitDetectionRadius : 2});
+    var ctx = document.getElementById("proxy_monitor").getContext("2d");
+    new Chart(ctx).Line({!! $pm !!}, {scaleLabel: function(object){return " " + object.value; }, pointHitDetectionRadius : 2});
 });
 </script>
 
+<h2>Proxy Error Type: (50 hours)</h2>
+<canvas id="proxy_error" width="1140" height="400"></canvas>
+<script type="text/javascript">
+$(document).ready(function() {
+    var ctx = document.getElementById("proxy_error").getContext("2d");
+    new Chart(ctx).Bar({!! $pe !!}, {scaleLabel: function(object){return " " + object.value; }});
+});
+</script>
 
 @endsection
