bf623c2f64c3e53e833d7232c97cc5124c56f8f6
RuiX <xrfind@gmail.com>
2015-11-24 17:14:03 +0100

    minor

--- resources/views/task/manage.blade.php
+++ resources/views/task/manage.blade.php
@@ -7,43 +7,46 @@
 @endsection
 
 @section('content')
+
 <div class="col-xs-6">
-<form class="form-horizontal" action="{{ action('TaskController@manageupdate') }}" method="POST">
-  <div class="form-group">
-    <label for="infoprob" class="col-sm-6 control-label">info Prob</label>
-    <div class="col-sm-6">
-      <input type="text" class="form-control" name="infoprob" value="{{ $info }}">
+{!! Form::open(['class' => "form-horizontal"]) !!}
+    <div class="form-group">
+        {!! Form::label('infoprob', 'info', ['class' => "col-sm-6 control-label"]) !!}
+        <div class="col-sm-6">
+            {!! Form::text('infoprob', $info, ['class' => "form-control"]) !!}
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="gainprob" class="col-sm-6 control-label">gain prob</label>
-    <div class="col-sm-6">
-      <input type="text" class="form-control" id="gainprob" value="{{ $gain }}">
+    <div class="form-group">
+        {!! Form::label('gainprob', 'gain', ['class' => "col-sm-6 control-label"]) !!}
+        <div class="col-sm-6">
+            {!! Form::text('gainprob', $gain, ['class' => "form-control"]) !!}
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="adjustprob" class="col-sm-6 control-label">adjust prob</label>
-    <div class="col-sm-6">
-      <input type="text" class="form-control" id="adjustprob" value="{{ $adjust_common }}">
+    <div class="form-group">
+        {!! Form::label('adjustprob', 'adjust', ['class' => "col-sm-6 control-label"]) !!}
+        <div class="col-sm-6">
+            {!! Form::text('adjustprob', $adjust_common, ['class' => "form-control"]) !!}
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="adjustlinkprob" class="col-sm-6 control-label">adjust list prob</label>
-    <div class="col-sm-6">
-      <input type="text" class="form-control" id="adjustlinkprob" value="{{ $adjust_list }}">
+    <div class="form-group">
+        {!! Form::label('adjustlprob', 'adjust list', ['class' => "col-sm-6 control-label"]) !!}
+        <div class="col-sm-6">
+            {!! Form::text('adjustlprob', $adjust_list, ['class' => "form-control"]) !!}
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="adjustcmplprob" class="col-sm-6 control-label">adjust complete prob</label>
-    <div class="col-sm-6">
-      <input type="text" class="form-control" id="adjustcmplprob" value="{{ $adjust_complete }}">
+    <div class="form-group">
+        {!! Form::label('adjustcprob', 'adjust comp', ['class' => "col-sm-6 control-label"]) !!}
+        <div class="col-sm-6">
+            {!! Form::text('adjustcprob', $adjust_complete, ['class' => "form-control"]) !!}
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-6 col-sm-10">
-      <button type="submit" class="btn btn-default">Update</button>
+    <div class="form-group">
+        <div class="col-sm-offset-6 col-sm-10">
+            {!! Form::submit('Update', ['class' => 'btn btn-default']) !!}
+        </div>
     </div>
-  </div>
-</form>
+{!! Form::close() !!}
 </div>
+
+
 @endsection
